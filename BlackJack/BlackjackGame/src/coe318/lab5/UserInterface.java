68549e9b3d7edf44f7722fdd5ccd1963e597861e
Kevin Dang <68084112+Kdang143@users.noreply.github.com>
2021-01-08 14:29:13 -0500

    Add files via upload

---
+++ BlackJack/BlackjackGame/src/coe318/lab5/UserInterface.java
@@ -0,0 +1,18 @@
+
+
+package coe318.lab5;
+
+/**
+ *
+ * @author kevin
+ */
+public interface UserInterface {
+   
+    public void setGame(BlackjackGame game);
+   
+    public void display();
+  
+    public boolean hitMe();
+    
+    public void gameOver();
+}
